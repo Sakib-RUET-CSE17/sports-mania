8260b5b570ae7e8c9692df849565e68e6c1f81f8
Sakib Hasan <hsakib8685@gmail.com>
2021-05-10 16:58:34 +0600

    href test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Footer = (props) => {
-    const {strFacebook,strTwitter,strWebsite,strYoutube}=props.league
+    // const {strFacebook,strTwitter,strWebsite,strYoutube}=props.league
+    const {strFacebook}=props.league
     console.log(strFacebook)
     return (
         <div className="text-center">
             <a href={strFacebook}><FontAwesomeIcon className="m-3" icon={faFacebook} size={'2x'} /></a>
-            <a href={""}><FontAwesomeIcon className="m-3" icon={faFacebook} size={'2x'} /></a>
-            <a href={""}><FontAwesomeIcon className="m-3" icon={faFacebook} size={'2x'} /></a>
+            {/* <a href={""}><FontAwesomeIcon className="m-3" icon={faFacebook} size={'2x'} /></a>
+            <a href={""}><FontAwesomeIcon className="m-3" icon={faFacebook} size={'2x'} /></a> */}
         </div>
     );
 };
